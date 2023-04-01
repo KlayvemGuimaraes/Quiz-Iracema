8482e03d38c1ac5793b3927a1a5636edba8c1d26
KlayvemG <klayvemguik@gmail.com>
2023-04-01 20:08:07 -0300

    Quiz-Iracema

---
+++ questions.js
@@ -0,0 +1,61 @@
+export default [
+    {
+      question: " (FUVEST 2009) Em um poema escrito em louvor de Iracema, Manuel Bandeira afirma que, ao compor esse livro, Alencar: <br> “[...] escreveu o que é mais poema Que romance, e poema menos Que um mito, melhor que Vênus.",
+      
+      answers: [
+        { option: "Alencar parte da ficção literária em direção à narrativa mítica, dispensando referências a coordenadas e personagens históricas.", correct: false },
+        { option: "O caráter poemático dado ao texto predomina sobre a narrativa em prosa, sendo, por sua vez, superado pela constituição de um mito literário.", correct: true },
+        { option: "A mitologia tupi está para a mitologia clássica, predominante no texto, assim como a prosa está para a poesia.", correct: false },
+      ],
+    },
+
+    {
+        question: "(UFU-MG) Sobre Iracema, de José de Alencar, podemos dizer que: 1) as cenas de amor carnal entre Iracema e Martim são de tal forma construídas que o leitor as percebe com vivacidade, porque tudo é narrado de forma explícita.2) em Iracema temos o nascimento lendário do Ceará, a história de amor entre Iracema e Martim e as manifestações de ódio das tribos tabajara e potiguara. 3) Moacir é o filho nascido da união de Iracema e Martim. De maneira simbólica ele representa o homem brasileiro, fruto do índio e do branco.4) a linguagem do romance Iracema é altamente poética, embora o texto esteja em prosa. Alencar consegue belos efeitos lingüísticos ao abusar de imagens sobre imagens, comparações sobre comparações.",     
+        answers: [
+          { option: "se apenas 2 e 4 estiverem corretas.", correct: false },
+          { option: "se apenas 2 e 3 estiverem corretas. ", correct: false },
+          { option: "se 2, 3 e 4 estiverem corretas. Leia mais:", correct: true },
+          { option: "se 1, 3 e 4 estiverem corretas.", correct: false},
+        ],
+      },
+
+    {
+      question: "(UVA 2020/2) - Da leitura de 'lracema', de José de Alencar, responda a questão. O pai de Iracema era:",
+      answers: [
+        { option: "Andira", correct: false },
+        { option: "Araquém", correct: true },
+        { option: "Irapuã", correct: false },
+        { option: "Caubi", correct: false },
+      ],
+    },
+    {
+      question: "(UESPI 2012) Romance escrito em 1865, Iracema, de José de Alencar, aborda fatos e feitos da colonização portuguesa no Brasil. Sobre esta obra, é correto afirmar que:",
+      answers: [
+        { option: " A estória se passa no século XVI, durante a exploração portuguesa no Amazonas.", correct: false },
+        { option: " A principal característica deste romance é que parte dele é escrito em prosa, outra parte em versos.", correct: false },
+        { option: " Apesar de ser um romance indigenista, Iracema também aborda com ênfase a questão da escravidão negra no Brasil.", correct: false },
+        { option: " Iracema é descrita por Alencar como virgem dos lábios de mel, com cabelos mais negros que a asa da graúna.", correct: true },
+        { option: " A principal característica de Iracema é a objetividade da narrativa, que exclui qualquer traço lírico ou subjetivo.", correct: false },
+
+      ],
+    },
+    {
+        question: "(UEL) Examine as proposições a seguir e assinale a alternativa INCORRETA",
+        answers: [
+          { option: " A relevância da obra de José de Alencar no contexto romântico decorre, em grande parte, da idealização dos elementos considerados como genuinamente brasileiros, notadamente a natureza e o índio. Essa atitude impulsionou o nacionalismo nascente, por ser uma forma de reação política, social e literária contra Portugal..", correct: false },
+          { option: " Ao lado de O guarani e Ubirajara, Iracema representa um mito de fundação do Brasil. Nessas obras, a descrição da natureza brasileira possui inúmeras funções, com destaque para a 'cor local', isto é, o elemento particular que o escritor imprimia à literatura, acreditando contribuir para a sua nacionalização. ", correct: true },
+          { option: " Embora tendo sido escrito no período romântico, Iracema apresenta traços da ficção naturalista tanto na criação das personagens quanto na tematização dos problemas do país.", correct: true },
+          { option: " A leitura de Iracema revela a importância do índio na literatura romântica. Entretanto, sabe-se que a presença do índio não se restringiu a esse contexto literário, tendo desembocado inclusive no Modernismo, por intermédio de escritores como Mário de Andrade e Oswald de Andrade.", correct: false},
+        ],
+      },
+    {
+      question: "(UEL 1998)No romance IRACEMA, José de Alencar",
+      answers: [
+        { option: " Desenvolve, na linha mesma do enredo, valores éticos e estéticos próprios da sociedade burguesa europeia.", correct: false },
+        { option: " Busca fundir num mesmo código as imagens de um lirismo romântico e alguns modos de nomeação e construção da língua tupi.", correct: true },
+        { option: " Defende a superioridade da cultura indígena sobre a europeia, tal como o demonstra o desfecho desse romance idealizante.", correct: false },
+        { option: " Faz confluírem o plano lendário e o plano histórico, o primeiro representado por Martim, e o segundo, por Iracema.", correct: false },
+        { option: " Dispõe-se a desenvolver a história de uma virgem que, resistindo ao colonizador, representa o poder da natureza indomável.", correct: false },
+      ],
+    },
+  ];
